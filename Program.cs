c3e73c0b1a5bed277e8ff42c62a89824047b54e5
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-08 20:36:35 +0530

    added EmployeeWageUC6

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWage_UC6 workingDaysHrs = new EmployeeWage_UC6();
+            workingDaysHrs.WorkingDaysHrs();
+        }
+    }
+}
