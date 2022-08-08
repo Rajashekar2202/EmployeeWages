ee97b8324b819599ceaa263190d5a429a4df3394
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-08 20:20:36 +0530

    added EmployeeWageUC4

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWage_UC5 monthlyWage = new EmployeeWage_UC5();
+            monthlyWage.MonthlyWage();
+        }
+    }
+}
