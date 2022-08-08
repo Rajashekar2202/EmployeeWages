7ab0a5ae7a2efae1d89af75a7d539c6ab12b87cf
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-08 19:28:57 +0530

    added EmployeeWageUC3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWage_UC3 partTimeEmployeeWage = new EmployeeWage_UC3();
+            partTimeEmployeeWage.PartTimeEmployeeWage();
+        }
+    }
+}
