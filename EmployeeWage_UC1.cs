09eb6eb5e26f7041f746d7a9afb631db515aa6eb
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-08 19:02:01 +0530

    added EmployeeWageUC1

---
+++ EmployeeWage_UC1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class EmployeeWage_UC1
+    {
+        public void EmployeePresentAbsent()
+        {
+            int IS_PRESENT = 1;      // Constant
+            Random random = new Random(); // Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//nsole.WriteLine("Hello, World!");
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeWage_UC1 employeePresentAbsent = new EmployeeWage_UC1();
+            employeePresentAbsent.EmployeePresentAbsent();
+        }
+    }
+}
